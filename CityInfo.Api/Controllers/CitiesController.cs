fd13064051097d1f4f9f3ff0be3af92c2ce0f9f1
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-01-31 13:38:48 +0100

    DTOs and controllers added; xml output support

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // check if city exists
+            var cityExists = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id);
+
+            if(cityExists == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityExists);
+        }
+    }
+}
